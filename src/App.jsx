14888f0c675ba4f7df193d74a7258ec98d52d91f
VinayThippani <thippanivinay633@gmail.com>
2024-07-01 22:20:54 +0530

    Heading-2

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App(){
   const buttons = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '+', '-', '*', '/', '%','=','C'];
   return(
     <center>
-      <h1>Calculator</h1>
+      <h1>Calculator Designed Using ReactJs</h1>
       <div className={styles.container}>
         <input type="text" className={styles.textBox}/>
         <Buttons buttonItems={buttons}/>
