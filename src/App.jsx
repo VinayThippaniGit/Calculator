c1b5bdb62e10bb67aa3f6d11b137589572221ae3
VinayThippani <thippanivinay633@gmail.com>
2024-07-01 22:12:11 +0530

    Heading change

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App(){
   const buttons = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '+', '-', '*', '/', '%','=','C'];
   return(
     <center>
+      <h1>Calculator</h1>
       <div className={styles.container}>
         <input type="text" className={styles.textBox}/>
         <Buttons buttonItems={buttons}/>
