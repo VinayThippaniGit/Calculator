100c271f971ecfc96667a7e67d7100a5e0da9889
VinayThippani <thippanivinay633@gmail.com>
2024-07-01 23:42:00 +0530

    Styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import Buttons from "./Buttons"
 
 function App(){
   
-  const buttons = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '+', '-', '*', '/', '%','=','C'];
+  const buttons = ['1', '2', '3', '4', '5', '6', '7', '8', '9','=', '0','C', '+', '-', '*', '/', '%'];
   return(
     <center>
-      <h1>Calculator Designed Using ReactJs</h1>
+      <h1>Calculator Using ReactJs</h1>
       <div className={styles.container}>
         <input type="text" className={styles.textBox}/>
         <Buttons buttonItems={buttons}/>
